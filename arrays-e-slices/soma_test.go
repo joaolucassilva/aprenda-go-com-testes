ad7b9fc86db94a891f3d6c3fba7e29db82b44008
joao <luccas332@gmail.com>
2022-12-28 20:41:25 -0300

    test: add tests for slices and arrays

--- arrays-e-slices/soma_test.go
+++ arrays-e-slices/soma_test.go
@@ -1,6 +1,9 @@
 package arrays_e_slices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSoma(t *testing.T) {
 	t.Run("coleção de 5 números", func(t *testing.T) {
@@ -23,5 +26,26 @@ func TestSoma(t *testing.T) {
 		if esperado != resultado {
 			t.Errorf("resultado %d, esperado %d, dado %v", resultado, esperado, numeros)
 		}
+	})
+}
+
+func TestSomaTodoOResto(t *testing.T) {
+	verificaSomas := func(t *testing.T, resultado, esperado []int) {
+		t.Helper()
+		if !reflect.DeepEqual(resultado, esperado) {
+			t.Errorf("resultado %v, esperado %v", resultado, esperado)
+		}
+	}
+
+	t.Run("faz a soma de alguns slices", func(t *testing.T) {
+		resultado := SomaTodoOResto([]int{1, 2}, []int{0, 9})
+		esperado := []int{2, 9}
+		verificaSomas(t, resultado, esperado)
+	})
+
+	t.Run("soma slices vazios de forma segura", func(t *testing.T) {
+		resultado := SomaTodoOResto([]int{}, []int{3, 4, 5})
+		esperado := []int{0, 9}
+		verificaSomas(t, resultado, esperado)
 	})
 }
